157e34eaef4106475b62581dc15cfd3813cd83c3
brianchesko <cheskobrian@gmail.com>
2019-04-10 12:29:40 -0400

    Created wrapper data class

--- Assignment4/src/Data.java
+++ Assignment4/src/Data.java
@@ -1,4 +1,27 @@
 public class Data
 {
-// encapsulated integer vertex and value d.f. and acccesors/mutators
+    private int value;
+    private int vertex;
+
+    public Data(int vertex, int value) {
+        this.value = value;
+        this.vertex = vertex;
+    }
+
+    public void setVertex(int vertex) {
+        this.vertex = vertex;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getVertex() {
+        return vertex;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
 }
